1907b614f845374349991f5fc901d962ea9f9a05
Max <88722440+Libes6@users.noreply.github.com>
2023-10-29 19:17:23 +0700

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,8 +1,9 @@
 window.addEventListener ("scroll",() => {
     const header =  document.querySelector('.header-transform')
-
+    const video = document.querySelector('.video-wrapper')
     if (window.scrollY>header.offsetHeight){
         header.classList.add("header-fix")
+        video.classList.add("video-wrapper-asynchronous")
     }
     else{
         header.classList.remove("header-fix")
